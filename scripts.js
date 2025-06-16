924eaddba17338f69a7f56a5e1b54fc9bc172339
Sávio Samuel <122576368+saviohsamuel@users.noreply.github.com>
2025-06-15 21:05:31 -0300

    Adicionando arquivos do projeto.

---
+++ scripts.js
@@ -0,0 +1,47 @@
+const submitButton = document.querySelector('.submit');
+const inputNewItem = document.querySelector('.new__item');
+const containerWrapper = document.querySelector('.container__wrapper');
+const dangerContainer = document.querySelector('.container__danger');
+const closeDanger = document.querySelector('.close__danger');
+const dangerItem = document.querySelector('.list__item');
+const deleteButtons = document.querySelectorAll('.delete');
+
+function addDeleteEvent(button) {
+    button.addEventListener('click', () => {
+        const item = button.closest('.input__wrapper');
+        const itemName = item.querySelector('label').innerText;
+        dangerItem.textContent = itemName;
+        item.remove();
+        dangerContainer.style.display = 'flex';
+        setTimeout(() => {
+            dangerContainer.style.display = 'none';
+        }, 6000);
+    });
+}
+deleteButtons.forEach(button => {
+    addDeleteEvent(button);
+});
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const itemText = inputNewItem.value.trim();
+    if (itemText !== '') {
+        const newItem = document.createElement('div');
+        newItem.classList.add('input__wrapper', 'flex', 'f-a-center');
+        newItem.innerHTML =
+        `
+            <div class="input flex f-a-center">
+                <input type="checkbox">
+                <label>${itemText}</label>
+            </div>
+            <button class="delete">
+                <img src="imgs/trash.svg" alt="Apagar">
+            </button>
+        `;
+        containerWrapper.prepend(newItem);
+        inputNewItem.value = '';
+        addDeleteEvent(newItem.querySelector('.delete'));
+    }
+});
+closeDanger.addEventListener('click', () => {
+    dangerContainer.style.display = 'none';
+});
